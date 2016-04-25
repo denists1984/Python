60a02efb799f33f80840c5a4cd0cc49743cbfc48
denists1984 <tseidl84@gmail.com>
2016-04-25 09:54:49 -0400

    add exercise_2

---
+++ factorial.py
@@ -0,0 +1,10 @@
+a = input('please enter number 8:')
+if (8-a != 0):
+    print "Wrong number exiting"
+        exit
+	else:
+	    const = 1
+	        for i in range(1,a+1):
+		        fact = const*i
+			print (fact)
+
