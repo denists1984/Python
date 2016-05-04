0687ce1253f03231149007cac03821b443db32f1
denists1984 <tseidl84@gmail.com>
2016-05-04 08:01:11 -0400

    add first class exercise

---
+++ first_class.py
@@ -0,0 +1,14 @@
+ass firstClass():
+    def __init__(self, a=10):
+    	self.my = a if a else "Bla Bla"
+
+
+def input(self):
+	self.my = raw_input("Enter Something : ")
+
+def printFU(self):
+	print self.my.upper()
+
+my = firstClass()
+my.input()
+my.printFU()
