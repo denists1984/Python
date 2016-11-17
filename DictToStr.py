4567d026111083ca3e538bd2d3365dcadc25ff31
denists1984 <tseidl84@gmail.com>
2016-11-16 20:35:19 -0500

    Create DictToStr.py

---
+++ DictToStr.py
@@ -0,0 +1,18 @@
+dicts = [
+    {'name': 'Michelangelo',
+     'food': 'PIZZA'},
+    {'name': 'Garfield',
+     'food': 'lasanga'},
+    {'name': 'Walter',
+     'food': 'pancakes'},
+    {'name': 'Galactus',
+     'food': 'worlds'}
+]
+
+string = "Hi, I'm {name} and I love to eat {food}!"
+
+def string_factory(dicts, string):
+    output = []
+    for i in dicts:
+        output.append(string.format(**i))
+    return output    
