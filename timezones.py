d63c0d4a0b200597ef00cac9a7255d8f9e6e65a1
denists1984 <tseidl84@gmail.com>
2016-12-13 20:57:09 -0500

    Create timezones.py

---
+++ timezones.py
@@ -0,0 +1,4 @@
+import datetime,time
+moscow = datetime.timezone(datetime.timedelta(hours =+ 4))
+pacific = datetime.timezone(datetime.timedelta(hours =- 8))
+india = datetime.timezone(datetime.timedelta(hours =+ 5, minutes =+ 30))
